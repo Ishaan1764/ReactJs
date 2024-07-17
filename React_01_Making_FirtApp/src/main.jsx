3ebe7c96aa487ae00ac936f7cf8e4cd1810984e8
Ishaan1764 <ishaansaluja1764@gmail.com>
2024-07-17 13:14:16 -0400

    learned how things work

--- React_01_Making_FirtApp/src/main.jsx
+++ React_01_Making_FirtApp/src/main.jsx
@@ -2,8 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <h1>YO YO</h1>
+  );
+}
+
+//vase yaha ek he element allowd hai pr humne use ek div mein kr deya jisse use ek he hoe pr usme bhut sare aa pae.
+//instead of<App /> we can also write it like App() because at the end yeh ek function he hai.
+
+//ab hum apna object kase render krvae?
+const reactElement=React.createElement(
+  'a',
+  {href:'https://google.com',target:'_blank'},
+  'Ajaoo google pe.'
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  reactElement
+  // <React.StrictMode>
+     
+  //   {/* <div>
+  //   <App />
+  //   <MyApp />
+   
+  //   </div> */}
+    
+  // </React.StrictMode>
 )
